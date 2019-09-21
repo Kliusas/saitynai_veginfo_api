89ac90f175ba0520a0b09c0956a84b98c28f7b16
Kliusas <karolis.kliucnikas@hostinger.com>
2019-09-21 15:13:20 +0300

    Fix Routes Naming like Design Pattern

--- routes/web.php
+++ routes/web.php
@@ -1,60 +1,70 @@
 <?php
 
-Route::get('dish-category/index', 'DishCategoryController@index');
-Route::post('dish-category/{id}/store', 'DishCategoryController@store');
-Route::delete('dish-category/{id}/delete', 'DishCategoryController@delete');
-Route::post('dish-category/{id}/update', 'DishCategoryController@update');
+Route::get('dishes-categories', 'DishCategoryController@index');
+Route::get('dishes-categories/{id}', 'DishCategoryController@index');
+Route::post('dishes-categories', 'DishCategoryController@store');
+Route::delete('dishes-categories/{id}', 'DishCategoryController@delete');
+Route::patch('dishes-categories/{id}', 'DishCategoryController@update');
 
 
-Route::get('dish/index', 'DishController@index');
-Route::post('dish/{id}/store', 'DishController@store');
-Route::delete('dish/{id}/delete', 'DishController@delete');
-Route::post('dish/{id}/update', 'DishController@update');
+Route::get('dishes', 'DishController@index');
+Route::get('dishes/{id}', 'DishController@index');
+Route::post('dishes', 'DishController@store');
+Route::delete('dishes/{id}', 'DishController@delete');
+Route::patch('dishes/{id}', 'DishController@update');
 
 
-Route::get('event-category/index', 'EventCategoryController@index');
-Route::post('event-category/{id}/store', 'EventCategoryController@store');
-Route::delete('event-category/{id}/delete', 'EventCategoryController@delete');
-Route::post('event-category/{id}/update', 'EventCategoryController@update');
+Route::get('event-categories', 'EventCategoryController@index');
+Route::get('event-categories/{id}', 'EventCategoryController@index');
+Route::post('event-categories', 'EventCategoryController@store');
+Route::delete('event-categories/{id}', 'EventCategoryController@delete');
+Route::patch('event-categories/{id}', 'EventCategoryController@update');
 
 
-Route::get('event/index', 'EventController@index');
-Route::post('event/{id}/store', 'EventController@store');
-Route::delete('event/{id}/delete', 'EventController@delete');
-Route::post('event/{id}/update', 'EventController@update');
+Route::get('events', 'EventController@index');
+Route::get('events/{id}', 'EventController@index');
+Route::post('events', 'EventController@store');
+Route::delete('events/{id}', 'EventController@delete');
+Route::patch('events/{id}', 'EventController@update');
 
 
-Route::get('ingredient/index', 'IngredientController@index');
-Route::post('ingredient/{id}/store', 'IngredientController@store');
-Route::delete('ingredient/{id}/delete', 'IngredientController@delete');
-Route::post('ingredient/{id}/update', 'IngredientController@update');
+Route::get('ingredients', 'IngredientController@index');
+Route::get('ingredients/{id}', 'IngredientController@index');
+Route::post('ingredients', 'IngredientController@store');
+Route::delete('ingredients/{id}', 'IngredientController@delete');
+Route::patch('ingredients/{id}', 'IngredientController@update');
 
 
-Route::get('product-category/index', 'ProductCategoryController@index');
-Route::post('product-category/{id}/store', 'ProductCategoryController@store');
-Route::delete('product-category/{id}/delete', 'ProductCategoryController@delete');
-Route::post('product-category/{id}/update', 'ProductCategoryController@update');
+Route::get('product-categories', 'ProductCategoryController@index');
+Route::get('product-categories/{id}', 'ProductCategoryController@index');
+Route::post('product-categories', 'ProductCategoryController@store');
+Route::delete('product-categories/{id}', 'ProductCategoryController@delete');
+Route::patch('product-categories/{id}', 'ProductCategoryController@update');
 
 
-Route::get('product/index', 'ProductController@index');
-Route::post('product/{id}/store', 'ProductController@store');
-Route::delete('product/{id}/delete', 'ProductController@delete');
-Route::post('product/{id}/update', 'ProductController@update');
+Route::get('products', 'ProductController@index');
+Route::get('products/{id}', 'ProductController@index');
+Route::post('products', 'ProductController@store');
+Route::delete('products/{id}', 'ProductController@delete');
+Route::patch('products/{id}', 'ProductController@update');
 
 
-Route::get('recipe/index', 'RecipeController@index');
-Route::post('recipe/{id}/store', 'RecipeController@store');
-Route::delete('recipe/{id}/delete', 'RecipeController@delete');
-Route::post('recipe/{id}/update', 'RecipeController@update');
+Route::get('recipes', 'RecipeController@index');
+Route::get('recipes/{id}', 'RecipeController@index');
+Route::post('recipes', 'RecipeController@store');
+Route::delete('recipes/{id}', 'RecipeController@delete');
+Route::patch('recipes/{id}', 'RecipeController@update');
 
 
-Route::get('restaurant/index', 'RestaurantController@index');
-Route::post('restaurant/{id}/store', 'RestaurantController@store');
-Route::delete('restaurant/{id}/delete', 'RestaurantController@delete');
-Route::post('restaurant/{id}/update', 'RestaurantController@update');
+Route::get('restaurants', 'RestaurantController@index');
+Route::get('restaurants/{id}', 'RestaurantController@index');
+Route::post('restaurants', 'RestaurantController@store');
+Route::delete('restaurants/{id}', 'RestaurantController@delete');
+Route::patch('restaurants/{id}', 'RestaurantController@update');
 
 
-Route::get('shop/index', 'ShopController@index');
-Route::post('shop/{id}/store', 'ShopController@store');
-Route::delete('shop/{id}/delete', 'ShopController@delete');
-Route::post('shop/{id}/update', 'ShopController@update');
+Route::get('shops', 'ShopController@index');
+Route::get('shops/{id}', 'ShopController@index');
+Route::post('shops', 'ShopController@store');
+Route::delete('shops/{id}', 'ShopController@delete');
+Route::patch('shops/{id}', 'ShopController@update');
