cb03cc24f76c6df371cd70edce4b8f2263f5b4af
Kliusas <karolis.kliucnikas@hostinger.com>
2019-12-07 02:27:23 +0100

    Implement JWT AUTH

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Redirect;
 use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
@@ -19,39 +21,35 @@ class LoginController extends Controller
     | to conveniently provide its functionality to your applications.
     |
     */
-
+    
     use AuthenticatesUsers;
-
+    
     /**
      * Where to redirect users after login.
-     *
      * @var string
      */
     protected $redirectTo = '/home';
-
+    
     /**
      * Create a new controller instance.
-     *
      * @return void
      */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
     }
-
+    
     /**
      * Redirect the user to the provider authentication page.
-     *
      * @return \Illuminate\Http\Response
      */
     public function redirectToProvider($driver)
     {
         return Socialite::driver($driver)->redirect();
     }
-
+    
     /**
      * Obtain the user information from provider.
-     *
      * @return \Illuminate\Http\Response
      */
     public function handleProviderCallback($driver)
@@ -61,11 +59,12 @@ class LoginController extends Controller
         } catch (\Exception $e) {
             return redirect()->route('login');
         }
-
+        
         $existingUser = User::where('email', $user->getEmail())->first();
-
+        
         if ($existingUser) {
-            auth()->login($existingUser, true);
+            return Redirect::to("http://localhost:8081/logged");
+            
         } else {
             $newUser                    = new User;
             $newUser->provider_name     = $driver;
@@ -76,10 +75,15 @@ class LoginController extends Controller
             $newUser->email_verified_at = now();
             $newUser->avatar            = $user->getAvatar();
             $newUser->save();
-
-            auth()->login($newUser, true);
+            
+            return Redirect::to("http://localhost:8081/logged");
         }
-
-        return redirect($this->redirectPath());
+        
+        return Redirect::to("http://localhost:8081/logged");
+    }
+    
+    public function logout()
+    {
+        auth()->logout();
     }
 }
